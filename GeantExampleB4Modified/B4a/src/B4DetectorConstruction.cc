165db763c338cbb8033edcf210efe903dd2fc3f4
navarrof <54847307+navarrof@users.noreply.github.com>
2019-09-20 09:13:27 +0200

    Add files via upload

---
+++ GeantExampleB4Modified/B4a/src/B4DetectorConstruction.cc
@@ -0,0 +1,209 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// 
+/// \file B4DetectorConstruction.cc
+/// \brief Implementation of the B4DetectorConstruction class
+
+#include "B4DetectorConstruction.hh"
+
+#include "G4Material.hh"
+#include "G4NistManager.hh"
+
+#include "G4Box.hh"
+#include "G4LogicalVolume.hh"
+#include "G4PVPlacement.hh"
+#include "G4PVReplica.hh"
+#include "G4GlobalMagFieldMessenger.hh"
+#include "G4AutoDelete.hh"
+
+#include "G4GeometryManager.hh"
+#include "G4PhysicalVolumeStore.hh"
+#include "G4LogicalVolumeStore.hh"
+#include "G4SolidStore.hh"
+
+#include "G4VisAttributes.hh"
+#include "G4Colour.hh"
+
+#include "G4PhysicalConstants.hh"
+#include "G4SystemOfUnits.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4ThreadLocal 
+G4GlobalMagFieldMessenger* B4DetectorConstruction::fMagFieldMessenger = nullptr; 
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B4DetectorConstruction::B4DetectorConstruction()
+ : G4VUserDetectorConstruction(),
+   fCaloPV(nullptr),
+   fGapPV(nullptr),
+   fCheckOverlaps(true)
+{
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B4DetectorConstruction::~B4DetectorConstruction()
+{ 
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4VPhysicalVolume* B4DetectorConstruction::Construct()
+{
+  // Define materials 
+  DefineMaterials();
+  
+  // Define volumes
+  return DefineVolumes();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void B4DetectorConstruction::DefineMaterials()
+{ 
+ 
+  // Vacuum
+  G4double densityvoid = 1.e-5*g/cm3;
+  G4double zvoid = 1.;
+  G4double avoid = 1.01*g/mole;
+  new G4Material("Galactic", zvoid , avoid ,densityvoid ,
+                  kStateGas, 2.73*kelvin, 3.e-18*pascal);
+
+  // Tungsten
+  G4double densityW = 19.3*g/cm3;
+  G4double aW = 183.85*g/mole;
+  G4double zW;
+  G4String name;
+  new G4Material(name="Tungsten", zW=74. , aW , densityW );
+ 
+
+  // Print materials
+  G4cout << *(G4Material::GetMaterialTable()) << G4endl;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4VPhysicalVolume* B4DetectorConstruction::DefineVolumes()
+{
+  // Geometry parameters
+  G4double calorSizeXY  = 10.*cm;
+  auto calorThickness = 10.*cm;
+  auto worldSizeXY = 1.2 * calorSizeXY;
+  auto worldSizeZ  = 1.2 * calorThickness; 
+  
+  // Get materials
+  auto defaultMaterial = G4Material::GetMaterial("Galactic");
+  auto absorberMaterial = G4Material::GetMaterial("Tungsten");
+  
+  if ( ! defaultMaterial || ! absorberMaterial ) {
+    G4ExceptionDescription msg;
+    msg << "Cannot retrieve materials already defined."; 
+    G4Exception("B4DetectorConstruction::DefineVolumes()",
+      "MyCode0001", FatalException, msg);
+  }  
+   
+  //     
+  // World
+  //
+  auto worldS 
+    = new G4Box("World",           // its name
+                 worldSizeXY/2, worldSizeXY/2, worldSizeZ/2); // its size
+                         
+  auto worldLV
+    = new G4LogicalVolume(
+                 worldS,           // its solid
+                 defaultMaterial,  // its material
+                 "World");         // its name
+                                   
+  auto worldPV
+    = new G4PVPlacement(
+                 0,                // no rotation
+                 G4ThreeVector(),  // at (0,0,0)
+                 worldLV,          // its logical volume                         
+                 "World",          // its name
+                 0,                // its mother  volume
+                 false,            // no boolean operation
+                 0,                // copy number
+                 fCheckOverlaps);  // checking overlaps 
+  
+  //                               
+  // Calorimeter
+  //  
+  auto calorimeterS
+    = new G4Box("Calorimeter",     // its name
+                 calorSizeXY/2, calorSizeXY/2, calorThickness/2); // its size
+                         
+  auto calorLV
+    = new G4LogicalVolume(
+                 calorimeterS,     // its solid
+                 absorberMaterial,  // its material
+                 "Calorimeter");   // its name
+
+  fCaloPV                              
+    = new G4PVPlacement(
+                 0,                // no rotation
+                 G4ThreeVector(),  // at (0,0,0)
+                 calorLV,          // its logical volume                         
+                 "Calorimeter",    // its name
+                 worldLV,          // its mother  volume
+                 false,            // no boolean operation
+                 0,                // copy number
+                 fCheckOverlaps);  // checking overlaps 
+  
+ 
+  //                                        
+  // Visualization attributes
+  //
+  worldLV->SetVisAttributes (G4VisAttributes::GetInvisible());
+
+  auto simpleBoxVisAtt= new G4VisAttributes(G4Colour(1.0,1.0,1.0));
+  simpleBoxVisAtt->SetVisibility(true);
+  calorLV->SetVisAttributes(simpleBoxVisAtt);
+
+  //
+  // Always return the physical World
+  //
+  return worldPV;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void B4DetectorConstruction::ConstructSDandField()
+{ 
+  // Create global magnetic field messenger.
+  // Uniform magnetic field is then created automatically if
+  // the field value is not zero.
+  G4ThreeVector fieldValue;
+  fMagFieldMessenger = new G4GlobalMagFieldMessenger(fieldValue);
+  fMagFieldMessenger->SetVerboseLevel(1);
+  
+  // Register the field messenger for deleting
+  G4AutoDelete::Register(fMagFieldMessenger);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
